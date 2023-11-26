79e7897fa86d65dcaa850e16fa658c5f8c333784
JewishLewish <lanbrokhman@gmail.com>
2023-11-25 17:13:37 -0800

    clean up

--- innovision.py
+++ innovision.py
@@ -1,35 +1,41 @@
 import streamlit as st
-import tensorflow
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
-# Open the file in read mode
-with open('requrements.txt', 'r') as file:
-    # Read the contents of the file
-    data = file.read()
 
-model = tf.keras.models.load_model('C:/Users/vikas/Downloads/neuralnetwork.h5')
 
-st.title("Burmese Handwriting to Text Recognition")
+def main():
+    # Open the file in read mode
+    #with open('requrements.txt', 'r') as file:
+    #    # Read the contents of the file
+    #    data = file.read()
+    # Why read requirements.txt? If you are trying to use it then it's better if you made try or except at the imports
 
-uploaded_file = st.file_uploader("Choose a handwritten Burmese text image", type=["jpg", "jpeg", "png"])
+    model = tf.keras.models.load_model('C:/Users/vikas/Downloads/neuralnetwork.h5')
 
-if uploaded_file is not None:
-    # Preprocess the image
-    img = image.load_img(uploaded_file, target_size=(100, 100))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    st.title("Burmese Handwriting to Text Recognition")
 
-    # Make prediction
-    prediction = model.predict(img_array)
+    uploaded_file = st.file_uploader("Choose a handwritten Burmese text image", type=["jpg", "jpeg", "png"])
 
-    # Display the uploaded image
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    if uploaded_file is not None:
+        # Preprocess the image
+        img = image.load_img(uploaded_file, target_size=(100, 100))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0) / 255.0
 
-    # Display the predicted class
-    st.subheader("Prediction:")
-    st.write(f"The predicted class is: {np.argmax(prediction)}")
+        # Make prediction
+        prediction = model.predict(img_array)
 
-    # Display the probabilities for each class
-    st.subheader("Class Probabilities:")
-    st.write(prediction)
+        # Display the uploaded image
+        st.image(img, caption="Uploaded Image", use_column_width=True)
+
+        # Display the predicted class
+        st.subheader("Prediction:")
+        st.write(f"The predicted class is: {np.argmax(prediction)}")
+
+        # Display the probabilities for each class
+        st.subheader("Class Probabilities:")
+        st.write(prediction)
+
+if __name__ == "__main__":
+    main()
